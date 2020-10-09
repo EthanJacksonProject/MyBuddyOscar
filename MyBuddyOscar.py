810bdfdc3a93adc107d13b4b067a898d6e162f82
EthanJacksonProject <39967826+EthanJacksonProject@users.noreply.github.com>
2020-10-08 22:38:59 -0400

    Add comments and fields for term, major

--- MyBuddyOscar.py
+++ MyBuddyOscar.py
@@ -3,6 +3,11 @@ from selenium.webdriver.common.keys import Keys
 
 from bs4 import BeautifulSoup
 
+# These have to match what Oscar has in the HTML
+term = 'fall 2020'
+major = 'Electrical & Computer Engr'
+
+# Ignores 100's of irrelevant courses - modify as needed
 blacklist = [
     "Thesis",
     "Special",
@@ -18,21 +23,20 @@ blacklist = [
 
 driver = webdriver.Chrome()
 try:
-    # Navigate to course page for Fall 2020 ECE 
     driver.get("https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_dyn_sched")
     assert "Select Term or Date Range" in driver.title
     elem = driver.find_element_by_name("p_term")
-    driver.find_element_by_xpath("//select[@name='p_term']/option[text()='Fall 2020']").click()
-    driver.find_element_by_xpath("//input[@value='Submit']").click()
-    driver.find_element_by_xpath("//select[@name='sel_subj']/option[text()='Electrical & Computer Engr']").click()
-    driver.find_element_by_xpath("//input[@value='Class Search']").click()
+    driver.find_element_by_xpath(f"//select[@name='p_term']/option[text()={term}]").click() # get term
+    driver.find_element_by_xpath("//input[@value='Submit']").click() # submit
+    driver.find_element_by_xpath(f"//select[@name='sel_subj']/option[text()={major}]").click() # select major
+    driver.find_element_by_xpath("//input[@value='Class Search']").click() # submit
 
-
+    # Pass the loaded html to beautifulSoup for parsing
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     courses = [element.text for element in soup.find_all(class_='ddtitle')]
-    courses = [course for course in courses if not any(substring in course for substring in blacklist)]
-    
+    courses = [course for course in courses if not any(substring in course for substring in blacklist)] # apply blacklist
+
     for course in courses:
         print(course)
     
