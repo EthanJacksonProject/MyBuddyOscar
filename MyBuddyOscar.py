e9d050ee96724fd0ee9c16d430143723d9787ae0
EthanJacksonProject <39967826+EthanJacksonProject@users.noreply.github.com>
2020-10-08 22:41:11 -0400

    Run black formatter and pylint

--- MyBuddyOscar.py
+++ MyBuddyOscar.py
@@ -1,11 +1,10 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-from bs4 import BeautifulSoup
-
 # These have to match what Oscar has in the HTML
-term = 'fall 2020'
-major = 'Electrical & Computer Engr'
+term = "fall 2020"
+major = "Electrical & Computer Engr"
 
 # Ignores 100's of irrelevant courses - modify as needed
 blacklist = [
@@ -18,28 +17,36 @@ blacklist = [
     "3710",
     "3043",
     "2031",
-    "2026"
-    ]
+    "2026",
+]
 
 driver = webdriver.Chrome()
 try:
     driver.get("https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_dyn_sched")
     assert "Select Term or Date Range" in driver.title
     elem = driver.find_element_by_name("p_term")
-    driver.find_element_by_xpath(f"//select[@name='p_term']/option[text()={term}]").click() # get term
-    driver.find_element_by_xpath("//input[@value='Submit']").click() # submit
-    driver.find_element_by_xpath(f"//select[@name='sel_subj']/option[text()={major}]").click() # select major
-    driver.find_element_by_xpath("//input[@value='Class Search']").click() # submit
+    driver.find_element_by_xpath(
+        f"//select[@name='p_term']/option[text()={term}]"
+    ).click()  # get term
+    driver.find_element_by_xpath("//input[@value='Submit']").click()  # submit
+    driver.find_element_by_xpath(
+        f"//select[@name='sel_subj']/option[text()={major}]"
+    ).click()  # select major
+    driver.find_element_by_xpath("//input[@value='Class Search']").click()  # submit
 
     # Pass the loaded html to beautifulSoup for parsing
     html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    courses = [element.text for element in soup.find_all(class_='ddtitle')]
-    courses = [course for course in courses if not any(substring in course for substring in blacklist)] # apply blacklist
+    soup = BeautifulSoup(html, "html.parser")
+    courses = [element.text for element in soup.find_all(class_="ddtitle")]
+    courses = [
+        course
+        for course in courses
+        if not any(substring in course for substring in blacklist)
+    ]  # apply blacklist
 
     for course in courses:
         print(course)
-    
+
 except Exception as e:
     print(f"Something went wrong! {e}")
     driver.close()
